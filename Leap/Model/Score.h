87ee1090121a46ca72ed4f3c7b1d0b2d072c4a5e
chakrit <chakrit@wisdomvast.com>
2014-05-10 14:51:24 +0700

    Score.h

---
+++ Leap/Model/Score.h
@@ -0,0 +1,13 @@
+//
+//  Score.h
+//  Leap
+//
+//  Created by Chakrit Paniam on 5/10/2557 BE.
+//  Copyright (c) 2557 Chakrit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Score : NSObject
+
+@end
---
+++ Leap/Model/Score.m
@@ -0,0 +1,13 @@
+//
+//  Score.m
+//  Leap
+//
+//  Created by Chakrit Paniam on 5/10/2557 BE.
+//  Copyright (c) 2557 Chakrit. All rights reserved.
+//
+
+#import "Score.h"
+
+@implementation Score
+
+@end
